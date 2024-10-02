5d8f0c96c11afac38370c90446aa9faf3507f157
Yaghsha <waseyaghsha@gamil.com>
2024-10-02 14:02:09 +0500

    header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,13 @@ import About from './Pages/About';
 import SignIn from './Pages/SignIn';
 import SignUp from './Pages/SignUp';
 import Profile from './Pages/Profile';
+import Header from './components/header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    {/* header */}
+    <Header/>
     <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/about" element={<About/>}/>
@@ -15,7 +18,7 @@ export default function App() {
     <Route path="/sign-up" element={<SignUp/>}/>
     <Route path="/profile" element={<Profile/>}/>
     </Routes>
-    </BrowserRouter>
+   </BrowserRouter>
 
   )
 }
