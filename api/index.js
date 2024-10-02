70d0cf6cea6a8ef28a8c3d1470c5838ec7eb48f8
Yaghsha <waseyaghsha@gamil.com>
2024-10-02 17:21:52 +0500

    create api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() =>{
@@ -14,3 +15,6 @@ const app = express();
 app.listen(5000, ()=>{
     console.log('Server listening on port 5000');
 });
+
+
+app.use("/api/user", userRouter);
