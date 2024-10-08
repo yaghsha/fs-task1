db6c6c186935f804b29749452320660c25210de1
Yaghsha <waseyaghsha@gamil.com>
2024-10-08 14:19:48 +0500

    create sign up

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() =>{
@@ -12,9 +13,12 @@ mongoose.connect(process.env.MONGO).then(() =>{
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(5000, ()=>{
     console.log('Server listening on port 5000');
 });
 
 
-app.use("/api/user", userRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
